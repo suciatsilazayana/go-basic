bae936f63113a192f263d1e195092061bbe19b40
suciatsila <sucizayana418@gmail.com>
2023-10-13 17:14:49 +0700

    Sesi 3 - Basic Syntax

---
+++ latihan-operator-logika/main.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+
+	gender := "male"
+	age := 17
+
+	// Penggunaan AND
+	if gender == "female" && age >= 21 {
+		println("Pelamar dapat dikerjakan")
+	} else {
+		println("Pelamar dapat dikerjakan")
+	}
+}
